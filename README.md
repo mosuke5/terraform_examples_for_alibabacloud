788d6f06d79f3101b9c3dcffc5dcf360acb99a29
mosuke5 <w.vamos603+github@gmail.com>
2017-05-17 13:28:21 +0900

    Add/rds sample (#2)
    * add database
    * add description rds sample

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ SLBを使った構成のサンプル。
 VPC内のWebサーバは、踏み台サーバを経由してインターネットに接続できるようにVRouterへのルーティング設定も行う。
 
 ![slb_sample](image/architecture_slb_sample.png)
+
+## rds_sample
+RDSを使った構成のサンプル。
+VPC内にWebサーバとRDSを構築。RDSへはWebサーバからのみアクセスできるようにホワイトリスト設定は。
+
+![rds_sample](image/architecture_rds_sample.png)
---
+++ rds_sample/terraform.tf
@@ -0,0 +1,86 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "zone" {}
+variable "database_user_name" {}
+variable "database_user_password" {}
+variable "database_name" {}
+variable "database_character" {}
+
+# Alicloud Providerの設定
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+# セキュリティグループの作成
+resource "alicloud_security_group" "sg" {
+  name   = "terraform-sg"
+  vpc_id = "${alicloud_vpc.vpc.id}" # セキュリティグループはVPCにひも付きます
+}
+
+resource "alicloud_security_group_rule" "allow_http" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "80/80"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.sg.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+# VPCの作成
+resource "alicloud_vpc" "vpc" {
+  name = "terraform-vpc"
+  cidr_block = "10.1.0.0/21"
+}
+
+# vswitchの作成。VPCの中に作ります。
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = "${alicloud_vpc.vpc.id}"
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = "${var.zone}"
+}
+
+# ECSに紐付けるEIP(グローバルIP)の作成
+resource "alicloud_eip" "eip" {
+  internet_charge_type = "PayByTraffic"
+}
+
+# 作成したEIPをECSと紐付けします
+resource "alicloud_eip_association" "eip_asso" {
+  allocation_id = "${alicloud_eip.eip.id}"
+  instance_id   = "${alicloud_instance.web.id}"
+}
+
+resource "alicloud_instance" "web" {
+  instance_name = "terraform-ecs"
+  availability_zone = "${var.zone}"
+  image_id = "m-6wec065ood8fic52mh6v" # CentOS7.3
+  instance_type = "ecs.n4.small"
+  io_optimized = "optimized"
+  system_disk_category = "cloud_efficiency"
+  security_groups = ["${alicloud_security_group.sg.id}"]
+  vswitch_id = "${alicloud_vswitch.vsw.id}"
+}
+
+resource "alicloud_db_instance" "rds" {
+    engine = "MySQL"
+    engine_version = "5.6"
+    db_instance_class = "rds.mysql.t1.small"
+    db_instance_storage = "10"
+    db_instance_net_type = "Intranet"
+    vswitch_id = "${alicloud_vswitch.vsw.id}"
+    security_ips  = ["${alicloud_instance.web.private_ip}"]
+
+    master_user_name = "${var.database_user_name}"
+    master_user_password = "${var.database_user_password}"
+
+    db_mappings = [{
+      db_name = "${var.database_name}"
+      character_set_name = "${var.database_character}"
+      db_description = "tf"
+    }]
+}
