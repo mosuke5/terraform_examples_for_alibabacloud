0eeda45be1d63c4bad48d85f88c0aa4c3ea3e382
mosuke5 <w.vamos603@gmail.com>
2017-05-17 13:26:59 +0900

    add description rds sample

--- rds_sample/terraform.tf
+++ rds_sample/terraform.tf
@@ -20,6 +20,17 @@ resource "alicloud_security_group" "sg" {
   vpc_id = "${alicloud_vpc.vpc.id}" # セキュリティグループはVPCにひも付きます
 }
 
+resource "alicloud_security_group_rule" "allow_http" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "80/80"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.sg.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
 # VPCの作成
 resource "alicloud_vpc" "vpc" {
   name = "terraform-vpc"
@@ -33,6 +44,28 @@ resource "alicloud_vswitch" "vsw" {
   availability_zone = "${var.zone}"
 }
 
+# ECSに紐付けるEIP(グローバルIP)の作成
+resource "alicloud_eip" "eip" {
+  internet_charge_type = "PayByTraffic"
+}
+
+# 作成したEIPをECSと紐付けします
+resource "alicloud_eip_association" "eip_asso" {
+  allocation_id = "${alicloud_eip.eip.id}"
+  instance_id   = "${alicloud_instance.web.id}"
+}
+
+resource "alicloud_instance" "web" {
+  instance_name = "terraform-ecs"
+  availability_zone = "${var.zone}"
+  image_id = "m-6wec065ood8fic52mh6v" # CentOS7.3
+  instance_type = "ecs.n4.small"
+  io_optimized = "optimized"
+  system_disk_category = "cloud_efficiency"
+  security_groups = ["${alicloud_security_group.sg.id}"]
+  vswitch_id = "${alicloud_vswitch.vsw.id}"
+}
+
 resource "alicloud_db_instance" "rds" {
     engine = "MySQL"
     engine_version = "5.6"
@@ -40,6 +73,7 @@ resource "alicloud_db_instance" "rds" {
     db_instance_storage = "10"
     db_instance_net_type = "Intranet"
     vswitch_id = "${alicloud_vswitch.vsw.id}"
+    security_ips  = ["${alicloud_instance.web.private_ip}"]
 
     master_user_name = "${var.database_user_name}"
     master_user_password = "${var.database_user_password}"
