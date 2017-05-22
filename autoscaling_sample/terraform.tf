4b6f037839bf8f00eb3349ce03a9bbe1ac07e2fb
mosuke5 <w.vamos603@gmail.com>
2017-05-22 18:53:38 +0900

    update

--- autoscaling_sample/terraform.tf
+++ autoscaling_sample/terraform.tf
@@ -35,18 +35,28 @@ resource "alicloud_security_group" "sg" {
   vpc_id = "${alicloud_vpc.vpc.id}" # セキュリティグループはVPCにひも付きます
 }
 
-## セキュリティグループのルール設定
-## 今回はWebサーバということで80番ポートのみ空けます
-#resource "alicloud_security_group_rule" "allow_http" {
-#  type              = "ingress"
-#  ip_protocol       = "tcp"
-#  nic_type          = "intranet"
-#  policy            = "accept"
-#  port_range        = "80/80"
-#  priority          = 1
-#  security_group_id = "${alicloud_security_group.sg.id}"
-#  cidr_ip           = "0.0.0.0/0"
-#}
+# セキュリティグループのルール設定
+resource "alicloud_security_group_rule" "allow_http" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "80/80"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.sg.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "22/22"
+  priority          = 2
+  security_group_id = "${alicloud_security_group.sg.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
 
 # VPCの作成
 resource "alicloud_vpc" "vpc" {
@@ -61,37 +71,37 @@ resource "alicloud_vswitch" "vsw" {
   availability_zone = "${var.zone}"
 }
 
-## vpc内サーバはfumidaiサーバを経由してインターネットに出れるようにルーティング設定
-## fumidaiサーバへのフォワーディング設定は別途必要
-#resource "alicloud_route_entry" "default" {
-#  router_id             = "${alicloud_vpc.vpc.router_id}"
-#  route_table_id        = "${alicloud_vpc.vpc.router_table_id}"
-#  destination_cidrblock = "0.0.0.0/0"
-#  nexthop_type          = "Instance"
-#  nexthop_id            = "${alicloud_instance.fumidai.id}"
-#}
-#
-## ECSに紐付けるEIP(グローバルIP)の作成
-#resource "alicloud_eip" "eip" {
-#  internet_charge_type = "PayByTraffic"
-#}
-#
-## 作成したEIPをECSと紐付けします
-#resource "alicloud_eip_association" "eip_asso" {
-#  allocation_id = "${alicloud_eip.eip.id}"
-#  instance_id   = "${alicloud_instance.fumidai.id}"
-#}
-#
-#resource "alicloud_instance" "fumidai" {
-#  instance_name = "terraform-ecs-fumidai"
-#  availability_zone = "${var.zone}"
-#  image_id = "centos_7_3_64_40G_base_20170322.vhd"
-#  instance_type = "ecs.n4.small"
-#  io_optimized = "optimized"
-#  system_disk_category = "cloud_efficiency"
-#  security_groups = ["${alicloud_security_group.sg.id}"]
-#  vswitch_id = "${alicloud_vswitch.vsw.id}"
-#}
+# vpc内サーバはfumidaiサーバを経由してインターネットに出れるようにルーティング設定
+# fumidaiサーバへのフォワーディング設定は別途必要
+resource "alicloud_route_entry" "default" {
+  router_id             = "${alicloud_vpc.vpc.router_id}"
+  route_table_id        = "${alicloud_vpc.vpc.router_table_id}"
+  destination_cidrblock = "0.0.0.0/0"
+  nexthop_type          = "Instance"
+  nexthop_id            = "${alicloud_instance.fumidai.id}"
+}
+
+# ECSに紐付けるEIP(グローバルIP)の作成
+resource "alicloud_eip" "eip" {
+  internet_charge_type = "PayByTraffic"
+}
+
+# 作成したEIPをECSと紐付けします
+resource "alicloud_eip_association" "eip_asso" {
+  allocation_id = "${alicloud_eip.eip.id}"
+  instance_id   = "${alicloud_instance.fumidai.id}"
+}
+
+resource "alicloud_instance" "fumidai" {
+  instance_name = "terraform-ecs-fumidai"
+  availability_zone = "${var.zone}"
+  image_id = "centos_7_3_64_40G_base_20170322.vhd"
+  instance_type = "ecs.n4.small"
+  io_optimized = "optimized"
+  system_disk_category = "cloud_efficiency"
+  security_groups = ["${alicloud_security_group.sg.id}"]
+  vswitch_id = "${alicloud_vswitch.vsw.id}"
+}
 
 resource "alicloud_ess_scaling_group" "scaling" {
   scaling_group_name = "terraform-scaling-group"
@@ -104,8 +114,11 @@ resource "alicloud_ess_scaling_group" "scaling" {
 
 resource "alicloud_ess_scaling_configuration" "config" {
   scaling_group_id  = "${alicloud_ess_scaling_group.scaling.id}"
-  image_id          = "centos_7_3_64_40G_base_20170322.vhd"
+  image_id          = "m-t4n9xcbgxmq3cl3cgb8c"
   instance_type     = "ecs.n1.tiny"
   io_optimized      = "optimized"
+  system_disk_category = "cloud_efficiency"
   security_group_id = "${alicloud_security_group.sg.id}"
+  active            = true
+  scaling_configuration_name = "terraform-scaling-conf"
 }
