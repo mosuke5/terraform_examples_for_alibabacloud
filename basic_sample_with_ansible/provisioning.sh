4e0ffbae7639659325a8c313b52c22b19e1ddee5
mosuke5 <w.vamos603+github@gmail.com>
2017-05-29 15:29:13 +0900

    Add/basic sample with ansible (#6)
    add basic_sample with ansible

---
+++ basic_sample_with_ansible/provisioning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum install -y wget epel
+yum install -y ansible
+cd /root
+wget https://raw.githubusercontent.com/mosuke5/terraform_for_alibabacloud_examples/add/ansible-basic-sample/basic_sample_with_ansible/playbook.yml
+ansible-playbook playbook.yml
