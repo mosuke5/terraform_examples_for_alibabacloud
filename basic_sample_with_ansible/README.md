d2f6f97b24a1b6b9e20cdc2976f31cd451a3e084
mosuke5 <w.vamos603@gmail.com>
2018-06-12 16:08:27 +0900

    Update README.md for basic_sample_with_ansible

--- basic_sample_with_ansible/README.md
+++ basic_sample_with_ansible/README.md
@@ -1,5 +1,39 @@
-# ベーシックサンプル(ansible利用)
-basic_sampleと構成は同様。
-basic_sampleはユーザデータ機能でシェルスクリプトを用いてhttpdのインストールや起動を行っていた。
-この例では、ユーザデータ機能でAnsibleを実行させてhttpdのインストールや起動を実施。
-これにより、シェルスクリプトでは表現しづらい複雑な設定などに対応が可能になる。
+# Basic example (with ansible)
+This is most basic example. This will create following resources. Architecture overview is [here](https://docs.google.com/presentation/d/1pqtbiJRGc3uUm8ulhMBf4SWm2WPCCrhgUInjm9DMYdc/edit?ts=5b1df94f#slide=id.g3c4891986d_1_0).
+This example use Ansible for server provisioning.
+
+1. Create VPC
+1. Create Vswitch
+1. Create Security Group and set some rules
+1. Create an ECS instance in Vswitch
+1. Create EIP and bind it to ECS instance
+1. Provision httpd to ECS instance with Userdata function
+
+## Advantege of using Ansible for server provisioning
+Userdata code supports shell script and cloud-init.
+But both isn't suitable for complex provisioning. When you provision complex and many configuration, I reccomend using provisioning tools like Ansible, Chef, Puppet and so on.
+
+## How to use
+First you need to chnage configuration to yours.
+```
+$ cp terraform.tfvars.sample terrafrom.tfvars
+$ vim terraform.tfvars
+ => Edit variables with your favorite editor.
+```
+
+Deploy to Alibaba Cloud
+```
+$ terraform apply
+Apply complete! Resources: 7 added, 0 changed, 0 destroyed.
+
+Outputs:
+
+eip = xx.xx.xx.xx
+```
+
+Access eip address with web browser.  
+![http_output](/basic_sample/http_output.png)
+
+
+## Reference(Japanese)
+http://qiita.com/mosuke5/items/a65683ce6569bffd7ef0
--- basic_sample_with_ansible/terraform.tf
+++ basic_sample_with_ansible/terraform.tf
@@ -1,26 +1,20 @@
-# ベースはbasic_sample。
-# ECSのユーザデータを利用してAnsibleを起動させるサンプル・
-
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
 variable "zone" {}
 variable "ecs_password" {}
 
-# Alicloud Providerの設定
 provider "alicloud" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region     = "${var.region}"
 }
 
-# セキュリティグループの作成
 resource "alicloud_security_group" "sg" {
   name   = "terraform-sg"
-  vpc_id = "${alicloud_vpc.vpc.id}" # セキュリティグループはVPCにひも付きます
+  vpc_id = "${alicloud_vpc.vpc.id}"
 }
 
-# セキュリティグループのルール設定
 resource "alicloud_security_group_rule" "allow_http" {
   type              = "ingress"
   ip_protocol       = "tcp"
@@ -32,40 +26,35 @@ resource "alicloud_security_group_rule" "allow_http" {
   cidr_ip           = "0.0.0.0/0"
 }
 
-# VPCの作成
 resource "alicloud_vpc" "vpc" {
-  name = "terraform-vpc"
+  name       = "terraform-vpc"
   cidr_block = "10.1.0.0/21"
 }
 
-# vswitchの作成。VPCの中に作ります。
 resource "alicloud_vswitch" "vsw" {
   vpc_id            = "${alicloud_vpc.vpc.id}"
   cidr_block        = "10.1.1.0/24"
   availability_zone = "${var.zone}"
 }
 
-# ECSに紐付けるEIP(グローバルIP)の作成
 resource "alicloud_eip" "eip" {
   internet_charge_type = "PayByTraffic"
 }
 
-# 作成したEIPをECSと紐付けします
 resource "alicloud_eip_association" "eip_asso" {
   allocation_id = "${alicloud_eip.eip.id}"
   instance_id   = "${alicloud_instance.web.id}"
 }
 
-# ECSの作成
 resource "alicloud_instance" "web" {
-  instance_name = "terraform-ecs"
-  availability_zone = "${var.zone}"
-  image_id = "centos_7_3_64_40G_base_20170322.vhd"
-  instance_type = "ecs.n4.small"
-  io_optimized = "optimized"
+  instance_name        = "terraform-ecs"
+  availability_zone    = "${var.zone}"
+  image_id             = "centos_7_3_64_40G_base_20170322.vhd"
+  instance_type        = "ecs.n4.small"
+  io_optimized         = "optimized"
   system_disk_category = "cloud_efficiency"
-  security_groups = ["${alicloud_security_group.sg.id}"]
-  vswitch_id = "${alicloud_vswitch.vsw.id}"
-  password   = "${var.ecs_password}"
-  user_data = "${file("provisioning.sh")}"
+  security_groups      = ["${alicloud_security_group.sg.id}"]
+  vswitch_id           = "${alicloud_vswitch.vsw.id}"
+  password             = "${var.ecs_password}"
+  user_data            = "${file("provisioning.sh")}"
 }
