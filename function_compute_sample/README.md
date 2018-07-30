b39b90e2dad6a68e684b0d605f2ca8ae78bfcf9a
mosuke5 <w.vamos603@gmail.com>
2018-07-30 20:33:08 +0900

    add function compute sample

---
+++ function_compute_sample/README.md
@@ -0,0 +1,21 @@
+# Function Compute example
+This is the basic function compute(FC) example. This will create following resources.
+This code create very simple function which output "hello world" every 1 minute.
+
+1. Create LogService Project and Store
+1. Create RAM role and policy for FC
+1. Create FC service, function, trigger
+
+## How to use
+First you need to chnage configuration to yours.
+```
+$ cp terraform.tfvars.sample terrafrom.tfvars
+$ vim terraform.tfvars
+ => Edit variables with your favorite editor.
+```
+
+Deploy to Alibaba Cloud
+```
+$ terraform apply
+Apply complete! Resources: 7 added, 0 changed, 0 destroyed.
+```
---
+++ function_compute_sample/terraform.tf
@@ -0,0 +1,93 @@
+variable "account_id" {}
+variable "access_key" {}
+variable "secret_key" {}
+varuable "oss_bucket" {}
+variable "region" {}
+variable "zone" {}
+
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  account_id = "${var.account_id}"
+  region = "${var.region}"
+}
+
+resource "alicloud_log_project" "example" {
+  name       = "fc-log-20180730"
+  description = "created by terraform"
+}
+
+resource "alicloud_log_store" "example" {
+  project = "${alicloud_log_project.example.name}"
+  name    = "fc-log-store"
+}
+
+resource "alicloud_ram_role" "role" {
+  name = "fc-role"
+  services = ["fc.aliyuncs.com"]
+  description = "this is a role test."
+  force = true
+}
+
+resource "alicloud_ram_policy" "policy" {
+  name = "fc-policy"
+  statement = [
+          {
+            effect = "Allow"
+            action = ["log:PostLogStoreLogs"]
+            resource = [
+              "acs:log:*:*:project/${alicloud_log_project.example.name}/logstore/${alicloud_log_store.example.name}"]
+          },
+          {
+            effect = "Allow"
+            action = ["oss:*"]
+            resource = ["*"]
+          }
+          ]
+  description = "this is a policy test"
+  force = true
+}
+
+resource "alicloud_ram_role_policy_attachment" "attach" {
+  policy_name = "${alicloud_ram_policy.policy.name}"
+  policy_type = "${alicloud_ram_policy.policy.type}"
+  role_name = "${alicloud_ram_role.role.name}"
+}
+
+resource "alicloud_fc_service" "foo" {
+  name = "my-fc-service"
+  description = "created by tf"
+  internet_access = false
+  role = "${alicloud_ram_role.role.arn}"
+  log_config = [
+    {
+      project = "${alicloud_log_project.example.name}"
+      logstore = "${alicloud_log_store.example.name}"
+    }
+  ]
+}
+
+resource "alicloud_fc_function" "foo" {
+  service = "${alicloud_fc_service.foo.name}"
+  name = "hello-world"
+  description = "tf unit test"
+  oss_bucket = "${var.oss_bucket}"
+  oss_key = "function_compute.py.zip"
+  memory_size = "512"
+  runtime = "python2.7"
+}
+
+resource "alicloud_fc_trigger" "foo" {
+  service = "${alicloud_fc_service.foo.name}"
+  function = "${alicloud_fc_function.foo.name}"
+  name = "hello-trigger"
+  type = "timer"
+  source_arn = "test"
+  config = <<EOF
+    {
+        "payload": "aaaaa",
+        "cronExpression": "0 0/1 * * * *",
+        "enable": true
+    }
+  EOF
+}
