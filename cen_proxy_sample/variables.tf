b1d4d7750ca531809bbcc08182a132cdce76c0b5
muffy <marufeuille@gmail.com>
2019-01-31 10:05:32 +0900

    add CEN example(with some bugs)

---
+++ cen_proxy_sample/variables.tf
@@ -0,0 +1,24 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region-a" {
+  default = "ap-northeast-1"
+}
+variable "region-b" {
+  default = "ap-northeast-1"
+}
+variable "cen_name" {}
+variable "cen_description" {}
+variable "vpc_name_region-a" {}
+variable "vpc_cidr_region-a" {}
+variable "vsw_cidr_region-a" {}
+variable "vpc_name_region-b" {}
+variable "vpc_cidr_region-b" {}
+variable "vsw_cidr_region-b" {}
+variable "vpn_gateway_name" {}
+variable "vpn_gateway_description" {}
+variable "ssl_vpn_server_name" {}
+variable "client_ip_pool" {}
+variable "ssl_vpn_client_cert_name" {}
+
+variable "zone_region-a" {}
+variable "zone_region-b" {}
